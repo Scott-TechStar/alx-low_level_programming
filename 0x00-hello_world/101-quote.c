8ad7fcdad7a17994880dba02951fa170fc1be359
John Mwadime <johnniekips@gmail.com>
2023-03-16 11:37:08 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
