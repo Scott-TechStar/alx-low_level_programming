290bf5e1a2a4dc6d87f0970fa7150f88da7ec7d0
John Mwadime <johnniekips@gmail.com>
2023-03-16 12:14:21 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Entry point.
  *
@@ -7,7 +6,6 @@
  *
  * Return: Always 1 (Success).
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
