f9387f465b15c9915ebd8811aa51d77b91ce08cf
Scott-TechStar <johnniekips@gmail.com>
2023-03-16 00:21:58 -0700

    c hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+#include<stdio.h>
+int main(void)
+{
+char sizechar;
+int sizeint;
+long sizelong;
+long long sizelonglong;
+float sizefloat;
+
+printf("Size of char: %lu byte(s)\n", sizeof(sizechar));
+printf("Size of int: %lu byte(s)\n", sizeof(sizeint));
+printf("Size of long int: %lu byte(s)\n", sizeof(sizelong));
+printf("Size of long long int: %lu byte(s)\n", sizeof(sizelonglong));
+printf("Size of float: %lu byte(s)\n", sizeof(sizefloat));
+
+return (0);
+}
+
