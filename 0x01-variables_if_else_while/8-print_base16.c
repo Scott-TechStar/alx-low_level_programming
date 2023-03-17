b4000ca0733cb08229df0175d7798c26e55c7e15
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 12:32:14 -0700

    m

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 char num;
-for(num = '0'; num <= '9'; num++)
+for (num = '0'; num <= '9'; num++)
 {
 putchar(num);
 }
-for(num = 'a'; num <= 'f'; num++)
-{		  
+for (num = 'a'; num <= 'f'; num++)
+{	  
 putchar(num);
 }
 putchar('\n');
