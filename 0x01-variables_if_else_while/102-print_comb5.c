104378404046c22816dbc5eb14dc8a3f90de557d
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 16:11:18 -0700

    scott

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,13 +17,9 @@ putchar((num2 / 10) + '0');
 putchar(' ');
 putchar((num1 / 10) + '0');
 putchar((num2 % 10) + '0');
-if ((num1 == 98) && (num2 == 99))
+if ((num1 != 98) && (num2 != 99))
 {
-putchar((num1 / 10) + '0');
-putchar((num2 / 10) + '0');
-putchar(' ');
-putchar((num1 % 10) + '0');
-putchar((num2 % 10) + '0');
+continue;
 }
 putchar(',');
 putchar(' ');
