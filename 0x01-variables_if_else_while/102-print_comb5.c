89deb728ce0637bd781bd4186ad6c0067d910c44
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 16:05:22 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /**
  * main - prints numbers 0 to 99.
  *
@@ -15,13 +13,17 @@ for (num1 = 0; num1 <= 98; num1++)
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
-putchar((num2 % 10) + '0');
+putchar((num2 / 10) + '0');
 putchar(' ');
 putchar((num1 / 10) + '0');
 putchar((num2 % 10) + '0');
 if ((num1 == 98) && (num2 == 99))
 {
-continue;
+putchar((num1 / 10) + '0');
+putchar((num2 / 10) + '0');
+putchar(' ');
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
 }
 putchar(',');
 putchar(' ');
