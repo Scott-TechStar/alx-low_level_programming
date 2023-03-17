75b6cc55b4295802834b3ab4d420f009e1d8c51f
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 16:36:25 -0700

    scott

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,23 @@ int main(void)
 {
 int num1;
 int num2;
-for (num1 = 0; num1 <= 99; num1++)
+for (num1 = 0; num1 < 100; num1++)
 {
-for (num2 = num1 + 1; num2 <= 99; num2++)
+for (num2 = 0; num2 < 100; num2++)
 {
-putchar((num1 / 10) + '0');
-putchar((num2 / 10) + '0');
+if (num1 < num2)
+{
+putchar((num1 / 10) + 48);
+putchar((num1 % 10) + 48);
 putchar(' ');
-putchar((num1 / 10) + '0');
-putchar((num2 % 10) + '0');
-if ((num1 == 99) && (num2 == 99))
+putchar((num2 / 10) + 48);
+putchar((num2 % 10) + 48);
+if ((num1 != 98) || (num2 != 99))
 {
-continue;
-}
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
