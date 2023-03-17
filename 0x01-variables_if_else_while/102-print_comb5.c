9bb1e7fa515df78cd05f758b93291bd3dad2ec3c
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 14:51:07 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int num1;
 int num2;
 for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = num1 + 1; num2 <= 99; num2++)
+for (num2 = 0; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
