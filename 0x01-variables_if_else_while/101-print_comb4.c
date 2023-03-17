63d5e91850e77613311cef2b0a3425067c3bd19f
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 13:28:21 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of 3 digits.
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+int i = 0;
+int m;
+int n;
+int o;
+while (i <= 999)
+{
+m = (c / 100 + '0');
+n = (i / 10 % 10 + '0');
+o = (i % 10 + '0');
+if ((m < n) && (n <o))
+{
+putchar(m);
+putchar(n);
+putchar(o);
+if (i != 789)
+{
+putchar(',');
+putchar(' ');
+}
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
