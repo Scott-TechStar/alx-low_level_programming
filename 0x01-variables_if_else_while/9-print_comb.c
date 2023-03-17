d8280203ef49df24293469edbfb94bb6f766d2e5
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 12:47:23 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Print all possible combination of numbers
+*
+* Return: Always 0(Success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
