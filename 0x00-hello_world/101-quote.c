4a790c422bf5fa931a5dff73804bf132b486e75b
Scott-TechStar <johnniekips@gmail.com>
2023-03-17 14:02:18 -0700

    quote -comb5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- and that piece of art is useful" -Dora Korpar, 2015-10-19'.
+ * main - Dora Korpar, 2015-10-19'
  *
- * Return: Always 1(success)
+ * Return: Always 0(Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
