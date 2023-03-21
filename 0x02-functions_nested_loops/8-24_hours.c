f65cdde7d70aa8ae8f5f4a1655207dd1bf303f9b
Scott-TechStar <johnniekips@gmail.com>
2023-03-21 05:47:04 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ for (hr1 = 48; hr1 <= 50; hr1++)
 {
 for (hr2 = 48; hr2 <= 57; hr2++)
 {
-for (min1 = 48; min1 <= 53 min1++)
+for (min1 = 48; min1 <= 53; min1++)
 {
-for (min2 = 48; min2 <= 57 min2++)
+for (min2 = 48; min2 <= 57; min2++)
 {
 if ((hr1 > 50) && (hr2 < 52))
 {
@@ -23,7 +23,7 @@ _putchar(hr1);
 _putchar(hr2);
 _putchar(58);
 _putchar(min1);
--putchar(min2);
+_putchar(min2);
 _putchar('\n');
 }
 }
