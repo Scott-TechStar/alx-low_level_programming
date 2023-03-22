e23cfd5fcc568f083d91fe0a708545ec59427a74
Scott-TechStar <johnniekips@gmail.com>
2023-03-22 03:40:51 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,9 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 
 /*while (i < 10)*/
@@ -19,5 +21,6 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
