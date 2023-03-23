cba39bb90f0464915f4577ab90b24da868471ac7
Scott-TechStar <johnniekips@gmail.com>
2023-03-23 06:37:15 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print 0-14 10times
+ *
+ * Return: null
+ */
+void more_numbers(void)
+{
+  char c;
+  int i;
+for (i = 0; i <= 10; i++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c > 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int mul(int a, int b);
 void _putchar(char);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /*MAIN_H*/
