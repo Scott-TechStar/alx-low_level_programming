8192fd99cd27e95f2b6b7eb45753ac5262c454c9
Scott-TechStar <johnniekips@gmail.com>
2023-03-23 05:53:06 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,10 @@ void print_numbers(void)
 int i;  
 if ((i >= 48) && (i <= 57))
 {
-for (i = 48; i < 57; i++)
+for (i = 48; i <= 57; i++)
 {
 _putchar(i);
 }
+_putchar('\n');
 }
-_putchar('\n'); 
 }
