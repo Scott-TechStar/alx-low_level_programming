2a35757caca729f931106d6a9d16a25e260f4cde
Scott-TechStar <johnniekips@gmail.com>
2023-03-23 12:45:14 -0700

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* print_number - prints an integer
-*
-* @n: Input integer
-*
-* Return: void
-*/
+ * print_number - prints an integer
+ *
+ * @n: Input integer
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
 int c, th;
