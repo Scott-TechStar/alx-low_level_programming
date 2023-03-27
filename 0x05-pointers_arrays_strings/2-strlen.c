10fce8c3e36fcf2e86345e574a426e5ff0ecc37f
Scott-TechStar <johnniekips@gmail.com>
2023-03-27 07:59:42 -0700

    trlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  * _strlen - Returns the length of a string.
- * @str: Input string
+ * @s: Input string
  *
  * Return: The length of @s.
  */
-int _strlen( char *s)
+int _strlen(char *s)
 {
 int  len = 0;
 while (*s++)
