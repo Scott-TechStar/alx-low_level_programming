4f6fd9a483e1cac173e465bbd28cbad40c7afc29
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 15:25:31 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ *
+ * Return: n value
+ */
+char *leet(char *n)
+{
+int i, len;
+char s4[] = "aAeEoOtTlL";
+char s5[] = "4433007711";
+for (i = 0; n[i] != '\0'; i++)
+{
+for (len = 0; len < 10; len++)
+{
+if (n[i] == s4[len])
+{
+n[i] = s5[len];
+}
+}
+}
+return (n);
+}
