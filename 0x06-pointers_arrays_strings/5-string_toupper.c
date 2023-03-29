dcab7765940c17a450cd82520b5cbce0195fce56
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 13:30:29 -0700

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *string_toupper - Changes all lowercase letters to uppercase.
- * @str: The string to be changed.
+ * @s: The string to be changed.
  *
  * Return: A pointer to the changed string.
  */
