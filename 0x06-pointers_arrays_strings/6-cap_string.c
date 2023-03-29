502094f9c3a102c13ea3bec839de7a31cbd63772
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 14:46:02 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,24 @@
 #include "main.h"
 /**
  * *cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * @n: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *str)
+char *cap_string(char *n)
 {
-int tmp;
-tmp = 0;
- if (!(str[0] >= 'a' && str[0] <= 'z'))
+int tmp = 0;
+while (n[tmp])
 {
-str[0] = str[0] - 32;
+while (!(n[tmp] >= 'a' && n[tmp] <= 'z'))
+{
+tmp++;
 }
-for (tmp = 0; str[tmp] != '\0'; tmp++)
+if (n[tmp - 1] == ' ' || n[tmp - 1] == '\t' || n[tmp- 1] == '\n' || n[tmp - 1] == ',' || n[tmp - 1] == ';' || n[tmp - 1] == '.' || n[tmp - 1] == '!' || n[tmp - 1] == '?' || n[tmp - 1] == '"' || n[tmp - 1] == '(' || n[tmp - 1] == ')' || n[tmp - 1] == '{' || n[tmp - 1] == '}' || tmp == 0)
 {
-switch (str[tmp])
-{
-case ',':
-case ';':
-case '.':
-case '!':
-case '?':
-case '"':
-case '(':
-case ')':
-case '{':
-case '}':
-case ' ':
-case '\n':
-case '\t':
-if (str[tmp + 1] > 96 && str(tmp + 1 < 123)
-{
-str[tmp + 1] = str[tmp + 1] - 32;
+n[tmp] -= 32;
 }
+tmp++;
 }
-}
-return (str);
+return (n);
 }
