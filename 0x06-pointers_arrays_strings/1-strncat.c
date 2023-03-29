d2b5576dcecda149960152715b937484448d0b75
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 12:15:18 -0700

    concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - Concatenates two strings
+ * *_strncat - Concatenates two strings
  *
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
@@ -10,14 +10,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, len = 0;
-for (len = 0; dest[i++]; len++)
+int i = 0;
+int len = 0;
+while (dest[i] != '\0')
 {
-continue;
+i++;
 }
-for (i = 0; src[i] && i < n; i++)
+while (len < n && src[len] != '\0')
 {
-dest[len++] = src[];
+dest[i] = src[len];
+i++;
+len++;
 }
+dest[i] = '\0';
 return (dest);
 }
