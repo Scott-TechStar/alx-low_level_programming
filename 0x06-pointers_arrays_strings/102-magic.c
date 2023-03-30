fff51e8236bafba904cea55775f6c84baf359ad3
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 22:26:56 -0700

    magiv

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
-/**
+a[2] = 1024;
+p = &n;
+/*
  * write your line of code here...
  * Remember:
  * - you are not allowed to use a
