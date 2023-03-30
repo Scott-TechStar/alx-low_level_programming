477231fad16feaa16e8fc51f2a2601f81b8eb943
Scott-TechStar <johnniekips@gmail.com>
2023-03-29 21:35:58 -0700

    capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @n: The string to be capitalized
  *
  * Return: A pointer to the changed string.
- */
+*/
 char *cap_string(char *n)
 {
 int len = 0, i;
@@ -14,7 +14,7 @@ char tmp[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 while (n[len])
 {
 i = 0;
-while (i <j)
+while (i < j)
 {
 if ((len == 0 || n[len - 1] == tmp[i]) && (n[len] >= 97 && n[len] <= 122))
 {
