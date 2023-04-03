af797603f15dc062401708a89471ac4f473ef45d
Scott-TechStar <johnniekips@gmail.com>
2023-04-03 02:58:25 -0700

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-int j = 0;
+int index = 0;
+int target = 0;
 while (n > 0)
 {
-dest[i] = src[j];
-i++;
-j++;
+dest[index] = src[target];
+index++;
+target++;
 n--;
 }
 return (dest);
