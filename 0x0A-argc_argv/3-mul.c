f2a84cccbbd6201447da543bc8dbd42f1c80d5a0
Scott-TechStar <johnniekips@gmail.com>
2023-04-08 11:21:45 -0700

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,13 +5,12 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
+ * Return: Returns  1 else 0.
  */
 int main(int argc, char *argv[])
 {
 int a, b, mul;
-if (argc <= 2)
+if (argc <= 3)
 {
 printf("Error\n");
 return (1);
