103b8f4c69ea16de9c7ed2f89c38d7b55e689d3f
Scott-TechStar <johnniekips@gmail.com>
2023-04-08 21:04:46 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b, mul;
+int a, b;
 if (argc <= 2 && argc != 0)
 {
 printf("Error\n");
 return (1);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-mul = a * b;
-printf("%d\n", mul);
+else
+{
+b = 1;
+for (a = 1; a < 3; a++)
+{
+b *= atoi(argv[a]);
+ printf("%d\n",  b)
+}
+}
 return (0);
 }
