c4abb71b798832e761930290354b085c2d6bf73d
John Mwadime <johnniekips@gmail.com>
2023-04-27 23:34:16 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    while (h != NULL) {
-        if (h->str != NULL)
-            printf("[%d] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
-        h = h->next;
-        count++;
-    }
-    return count;
+size_t count = 0;
+while (h != NULL) {
+if (h->str != NULL)
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+h = h->next;
+count++;
+}
+return count;
 }
 
 
