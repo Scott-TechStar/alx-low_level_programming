b6a9ccca260ff41ba0bd01cbbc0255fcdf80d9bc
Scott-TechStar <johnniekips@gmail.com>
2023-04-27 12:40:34 -0700

    Updated the files

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+/**
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+size_t elmts = 0;
+while (h)
+{
+elmts++;
+h = h->next;
+}
+return (elmts);
+}
