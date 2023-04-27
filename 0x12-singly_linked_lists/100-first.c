10204a378a385ee5cd88b9122515f73f540adf5f
Scott-TechStar <johnniekips@gmail.com>
2023-04-27 13:21:42 -0700

    optional

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void __attribute__((constructor)) hare(void);
+/**
+ * prnt_str - Prints a string before the
+ *        main function is executed.
+ */
+void prnt_str(void)
+{
+printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
