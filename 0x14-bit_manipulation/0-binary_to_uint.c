3955d503dd57a491ba559dd3951a28cbf974b8fb
Scott-TechStar <johnniekips@gmail.com>
2023-05-04 18:58:33 -0700

    Bit Mani

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,33 @@
 #include "main.h"
 /**
- * _stoi - converts chars to ints
- * @c: char to convert
- * Return: converted int
- */
-unsigned int _stoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-/**
- * _strlen - calculates the length of the string
- * @s: input
- * Return: length of string
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-/**
- * binary_to_uint - converts a string of 1's and 0's to a decimal number
- * @b: string to convert
- * Return: unsigned decimal number
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int result, tmp, expo;
-	if (!b)
-		return (0);
-	result = tmp = 0;
-	expo = 1;
-	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		tmp = _stoi(b[i]);
-		result += tmp * expo;
-	}
-	return (result);
+int i;
+unsigned int j;
+j = 0;
+if (!b)
+{
+return (0);
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
+}
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+j <<= 1;
+if (b[i] == '1')
+{
+j += 1;
+}
+}
+return (j);
+}
