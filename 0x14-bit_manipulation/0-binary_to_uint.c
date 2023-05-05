92a0ba70e2ceb28b066efb98759e4379410f27ee
John Mwadime <johnniekips@gmail.com>
2023-05-05 04:32:26 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,43 @@
 #include "main.h"
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
+ */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0, mult = 1;
-int len;
-if (b == '\0')
-{
-return (0);
-}
-for (len = 0; b[len];)
-{
-len++;
-}
-for (len -= 1; len >= 0; len--)
-{
-if (b[len] != '0' && b[len] != '1')
-{
-return (0);
-}
-num += (b[len] - '0') * mult;
-mult *= 2;
-}
-return (num);
-}
+	int i;
+	unsigned int result, tmp, expo;
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
+	}
+	return (result);
