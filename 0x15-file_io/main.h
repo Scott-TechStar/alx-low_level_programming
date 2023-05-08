5b78e8063605e108efffa966f460172a6ccee12d
Scott-TechStar <johnniekips@gmail.com>
2023-05-08 16:04:20 -0700

    File I/0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+
+
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+
+
+
+#endif
