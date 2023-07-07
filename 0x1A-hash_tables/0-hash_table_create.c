a9b1d5c02837435b799a45163736958033baad27
John Mwadime <johnniekips@gmail.com>
2023-07-07 16:44:24 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,20 +13,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (size == 0)
-	return (NULL);
-	ht = malloc(sizeof(hash_table_t));
+		return (NULL);
 	if (ht == NULL)
 		return (NULL);
-
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	ht->array = (hash_node_t**)malloc(size * sizeof(hash_node_t*));
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
+	ht->size = size;
 	return (ht);
 }
